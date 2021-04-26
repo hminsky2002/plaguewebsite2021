540eefbf0db4e638df15560f7168c53280cc74cd
hminsky2341 <harry.minsky@gmail.com>
2021-04-26 00:21:40 -0400

    Merged w new pieces, fixed style sourcing errors, fixed sidebar errors

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ function myFunction() {
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("body").style.marginLeft = "250px";
+  document.getElementById("openbtn").style.visibility = "hidden";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("body").style.marginLeft= "0";
+  document.getElementById("openbtn").style.visibility = "visible";
 }
